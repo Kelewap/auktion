3d9adefaf6013868451fd2c833aa121f6a7011e4
Pawel Mikolajczyk <pawel.b.mikolajczyk@gmail.com>
2014-12-08 00:27:41 +0100

    [Lab 5] Persist auction duration to be available at recovery

--- src/main/scala/auktion/Auction.scala
+++ src/main/scala/auktion/Auction.scala
@@ -1,8 +1,10 @@
 package auktion
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorRef, FSM, Props}
 import akka.event.LoggingReceive
-import akka.persistence.PersistentActor
+import akka.persistence.{RecoveryCompleted, PersistentActor}
 
 import scala.concurrent.duration.FiniteDuration;
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -22,11 +24,15 @@ class Auction(bidTime: FiniteDuration, deleteTime: FiniteDuration, owner: ActorR
 
   val INITIAL_BID = 10
   override def persistenceId = "persistent-auction-001"
+  var lastTick: Long = System.currentTimeMillis()
 
+  var duration: Long = 0
   var data: AuctionData = AuctionData(null, INITIAL_BID)
 
   def updateState(event: StateChangeEvent): Unit = {
+//    println(event)
     data = event.data
+    duration = event.duration
     context.become(
       event.state match {
         case Created => created
@@ -36,53 +42,72 @@ class Auction(bidTime: FiniteDuration, deleteTime: FiniteDuration, owner: ActorR
     })
   }
 
+  def updateTimer(): Unit = {
+    duration += System.currentTimeMillis() - lastTick
+    lastTick = System.currentTimeMillis()
+  }
+
   def created: Receive = LoggingReceive {
     case BidTimerExpired => {
-      persist(StateChangeEvent(Ignored, data)) {
+      persist(StateChangeEvent(Ignored, data, duration)) {
         event => {
 //          log.debug("EXPIRED")
           updateState(event)
+          updateTimer()
         }
       }
     }
 
     case Bid(value) if value > INITIAL_BID => {
-      persist(StateChangeEvent(Activated, AuctionData(sender, value))) {
+      persist(StateChangeEvent(Activated, AuctionData(sender, value), duration)) {
         event => {
           println(self.path.name + " received valid initial bid: " + value + " from " + sender.path.name)
           updateState(event)
+          updateTimer()
         }
       }
     }
 
     case Bid(value) => {
 //      log.debug("received ignored bid: {}", value)
+    }
+
+    case RecoveryCompleted => {
+      val newBidTimer = FiniteDuration(bidTime.toMillis - duration, TimeUnit.MILLISECONDS)
+      context.system.scheduler.scheduleOnce(newBidTimer, context.self, BidTimerExpired)
     }
   }
 
   def activated: Receive = LoggingReceive {
     case BidTimerExpired => {
-      persist(StateChangeEvent(Sold, data)) {
+      persist(StateChangeEvent(Sold, data, duration)) {
         event => {
           data.bestBidder ! Bought("item of " + self.path.name)
           owner ! AuctionEnded
           context.system.scheduler.scheduleOnce(deleteTime, context.self, DeleteTimerExpired)
           updateState(event)
+          updateTimer()
         }
       }
     }
 
     case Bid(value) if value > data.bestPrice => {
-      persist(StateChangeEvent(Activated, AuctionData(sender, value))) {
+      persist(StateChangeEvent(Activated, AuctionData(sender, value), duration)) {
         event => {
           println(self.path.name + " received valid bid: " + value + " from " + sender.path.name)
           updateState(event)
+          updateTimer()
         }
       }
     }
 
     case Bid(value) => {
 //      log.debug("received ignored bid: {}", value)
+    }
+
+    case RecoveryCompleted => {
+      val newBidTimer = FiniteDuration(bidTime.toMillis - duration, TimeUnit.MILLISECONDS)
+      context.system.scheduler.scheduleOnce(newBidTimer, context.self, BidTimerExpired)
     }
   }
 
@@ -94,10 +119,11 @@ class Auction(bidTime: FiniteDuration, deleteTime: FiniteDuration, owner: ActorR
 
   def ignored: Receive = LoggingReceive {
     case Relist => {
-      persist(StateChangeEvent(Activated, data)) {
+      persist(StateChangeEvent(Activated, data, duration)) {
         event => {
           context.system.scheduler.scheduleOnce(bidTime, context.self, BidTimerExpired)
           updateState(event)
+          updateTimer()
         }
       }
     }
@@ -105,6 +131,11 @@ class Auction(bidTime: FiniteDuration, deleteTime: FiniteDuration, owner: ActorR
 
   override def receiveRecover: Receive = {
     case evt: StateChangeEvent => updateState(evt)
+
+    case RecoveryCompleted => {
+      val newBidTimer = FiniteDuration(bidTime.toMillis - duration, TimeUnit.MILLISECONDS)
+      context.system.scheduler.scheduleOnce(newBidTimer, context.self, BidTimerExpired)
+    }
   }
 
   override def receiveCommand: Receive = created
@@ -114,4 +145,4 @@ sealed trait Data
 case object Uninitialized extends Data
 case class AuctionData(bestBidder:ActorRef, bestPrice:Int) extends Data
 
-case class StateChangeEvent(state: State, data: AuctionData)
+case class StateChangeEvent(state: State, data: AuctionData, duration: Long)
